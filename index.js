f5b41499a0ecb655c3ef7c6cdaad0dbd00f176e2
David <davidkuron1996@gmail.com>
2025-09-02 15:07:00 +0300

    adds this file

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+window.saturdayFun = saturdayFun; 
+global.saturdayFun = saturdayFun;
+
+
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+global.saturdayFun = saturdayFun;
+
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+global.mondayWork = mondayWork;
+global.mondayWork = mondayWork;
+
+function wrapAdjective(highlight = "*") {
+  return function(adjective = "special") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
+global.wrapAdjective = wrapAdjective;
+
+function wrapAdjective(highlight = "*") {
+  return function(adjective = "special") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
+global.wrapAdjective = wrapAdjective;
+
+function wrapAdjective(highlight = "*") {
+  return function(adjective = "special") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
+global.wrapAdjective = wrapAdjective;
+
+function wrapAdjective(highlight = "*") {
+  return function(adjective = "special") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
+global.wrapAdjective = wrapAdjective;
+
+
+
+
+
